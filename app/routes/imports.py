ccb2d4f6a59a6b6c9c3496066a4dcc3217a25b45
index_object <index_object@outlook.com>
2026-06-14 22:16:34 +0800

    fix: restore secure_filename to prevent path traversal

--- app/routes/imports.py
+++ app/routes/imports.py
@@ -88,7 +88,7 @@ def upload():
         flash("请选择文件")
         return redirect(url_for("imports.index"))
 
-    filename = file.filename
+    filename = secure_filename(file.filename)
     ext = os.path.splitext(filename)[1].lower()
     if ext not in (".xlsx", ".xls"):
         flash("仅支持 .xlsx / .xls 文件")
